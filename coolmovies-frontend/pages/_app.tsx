f440a7d4da4328a55b0466f12c89939c6ff42a39
Vitor Diorio <vitor.diorio@yahoo.com.br>
2024-08-19 12:27:58 -0300

    ci(env): Add GraphQL URL to .env

--- coolmovies-frontend/pages/_app.tsx
+++ coolmovies-frontend/pages/_app.tsx
@@ -1,31 +1,34 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import React, { FC, useState } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import Head from 'next/head';
-import { createStore } from '../redux';
-import { EnhancedStore } from '@reduxjs/toolkit';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React, { FC, useState } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import Head from "next/head";
+import { createStore } from "../redux";
+import { EnhancedStore } from "@reduxjs/toolkit";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const [store, setStore] = useState<EnhancedStore | null>(null);
   React.useEffect(() => {
+    const uri = process.env.NEXT_PUBLIC_GRAPHQL_URL || "/graphql";
     const client = new ApolloClient({
       cache: new InMemoryCache(),
-      uri: '/graphql',
+      uri,
     });
 
     const store = createStore({ epicDependencies: { client } });
     setStore(store);
   }, []);
-  if (!store) return <>{'Loading...'}</>;
+
+  if (!store) return <>{"Loading..."}</>;
+
   return (
     <>
       <Head>
-        <title>{'Coolmovies Frontend'}</title>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>{"Coolmovies Frontend"}</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <ReduxProvider store={store}>
         <Component {...pageProps} />
